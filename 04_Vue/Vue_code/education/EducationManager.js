65df6ec57b5413bf187da1d9a04a69d91923188e
Kyunghwan Jeon <crasheso@gmail.com>
2025-03-17 21:55:19 +0900

    실습 5번 clear

--- 04_Vue/Vue_code/education/EducationManager.js
+++ 04_Vue/Vue_code/education/EducationManager.js
@@ -1,8 +1,45 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
 class EducationManager{
     constructor(){
         this.edu = [];
     }
     registerProgram = function(education){
         this.edu.push(education);
+        console.log(`[등록 완료] 교육 과정: ${education.title}`);
+    }
+    completeProgramWithFunction =function(string){
+        this.edu.forEach(function(tmp){
+            if(tmp.title === string){
+                tmp.isCompleted = true;
+                console.log(`[이수 완료 - 일반 함수] ${tmp.title}`);
+            }
+        })
+    }
+    completeProgramWithArrowFunction = function(string){
+        this.edu.forEach(tmp => {
+            if(tmp.title === string){
+                tmp.isCompleted = true;
+                console.log(`[이수 완료 - 화살표 함수] ${tmp.title}`);
+            }
+        });
+    }
+    saveToFile = async function() {
+        try{
+            const filePath = path.join(__dirname, 'education_records.json');
+            await fs.writeFile(filePath, JSON.stringify(this.edu, null, 2), 'utf-8');
+            console.log("[파일 저장 완료] 교육 기록이 파일로 저장되었습니다.");
+        }
+        catch{
+            console.error("[저장 오류]", error);
+        }
+        
     }
 }
+export {EducationManager};
--- 04_Vue/Vue_code/education/EducationProgram.js
+++ 04_Vue/Vue_code/education/EducationProgram.js
@@ -2,5 +2,7 @@ class EducationProgram{
     constructor(title, description){
         this.title = title;
         this.description = description;
+        this.isCompleted = false;
     }
 }
+export {EducationProgram};
